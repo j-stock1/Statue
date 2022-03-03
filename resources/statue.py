751765975a8227c45d30e823c57036e9725965cd
Logan <kingofallcreepers@gmailc.com>
2022-03-02 19:26:38 -0500

    Added the ability to remap the connections of the crystals.

--- resources/statue.py
+++ resources/statue.py
@@ -1,5 +1,4 @@
 import serial
-import time
 import numpy
 
 
@@ -76,15 +75,16 @@ class Statue:
     rings = 17
     crystals = 22
 
-    def __init__(self):
+    def __init__(self, dummyConnection=False, ):
+        self.port = "COM6"
+        self.baudrate = 115200
+
         self.serial = None
-        #self.serial = serial.Serial(port='COM6', baudrate=115200, timeout=0.1)
+        if not dummyConnection:
+            self.create_connection()
         self.state = numpy.zeros((self.rings, self.crystals, 3))
+        self.convertMap = numpy.zeros((self.rings, self.crystals), dtype="int")
         self.exportedState = numpy.zeros((self.muxSize * 2, self.muxSize, 3), dtype=">i1")
-        self.exportedState[0][0][0] = 1
-        self.exportedState[0][0][1] = 2
-        self.exportedState[0][0][2] = 3
-        print(self.state.shape)
 
     @allow_rgb
     def set_column(self, column, color):
@@ -106,8 +106,85 @@ class Statue:
             for crystal in range(self.crystals):
                 self.state[ring][crystal] = color.get_tuple()
 
+    def is_connected(self):
+        if self.serial is None:
+            return False
+        return self.serial.isOpen()
+
+    def create_connection(self):
+        if not self.is_connected():
+            try:
+                self.serial = serial.Serial(port=self.port, baudrate=self.baudrate, timeout=0.1)
+            except serial.serialutil.SerialException:
+                pass
+
+    def close_connection(self):
+        if self.is_connected():
+            self.serial.close()
+            self.serial = None
+
+    def transform_colors(self):
+        for ring in range(self.rings):
+            for crystal in range(self.crystals):
+                index = self.convertMap[ring][crystal]
+                muxX = index % self.muxSize
+                muxY = index // self.muxSize
+                self.exportedState[muxY][muxX] = self.state[ring][crystal]
+
+    def load_conversion_map(self, filePath):
+        offset = 1, 3
+        size = 44, 17
+
+        try:
+            with open(filePath) as f:
+                data = f.read()
+        except IOError:
+            return False
+
+        lines = data.split("\n")
+
+        data_array = []
+
+        for line_index, line in enumerate(lines):
+            if offset[1] <= line_index <= offset[1] + size[1]:
+                data_points = line.split(",")
+
+                array_row = []
+                dp = []
+
+                for data_point_index, data_point in enumerate(data_points):
+                    if offset[0] <= data_point_index <= offset[0] + size[0]:
+                        if len(dp) > 0 and dp[0] == -1:
+                            dp.append(-1)
+                            array_row.append(dp)
+                            dp = []
+                            continue
+                        try:
+                            dp.append(int(data_point))
+                        except ValueError:
+                            dp.append(-1)
+
+                        if len(dp) == 2:
+                            array_row.append(dp)
+                            dp = []
+                data_array.append(array_row)
+        data_array.reverse()
+        for y in range(len(data_array)):
+            for x in range(len(data_array[y])):
+                x_value = data_array[y][x][1]
+                y_value = data_array[y][x][0]
+                if x_value == -1:
+                    c = -1
+                else:
+                    c = y_value * self.crystals + x_value
+                self.convertMap[y][x] = c
+
+        return True
+
     def update_statue(self):
+        self.transform_colors()
         output = numpy.ndarray.tobytes(self.exportedState)
+
         if self.serial:
             self.serial.write(output)
 
@@ -115,3 +192,6 @@ class Statue:
                 recv = self.serial.read(1)
                 if len(recv) == 1:
                     break
+
+    def upload_code(self, cppFile):
+        pass
