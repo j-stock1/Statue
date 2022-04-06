a9faf593e569c440d4e04e18e6ef316daeabed4f
Logan Modic <l-modic@onu.edu>
2022-04-05 22:03:06 -0400

    fixed js issues

--- sources/js/script.js
+++ sources/js/script.js
@@ -4,16 +4,16 @@ import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
 import { OutlinePass } from 'three/examples/jsm/postprocessing/OutlinePass.js';
-const parent = document.getElementById("panel2")
+const parent = document.getElementById("panel2");
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(45, parent.offsetWidth/parent.offsetHeight, 1, 1000);
-const renderer = new THREE.WebGLRenderer();
-const composer = new EffectComposer( renderer );
+const camera = new THREE.PerspectiveCamera(45, parent.offsetWidth/parent.offsetHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({alpha: true, antialias: true});
+renderer.setClearColor(0xffffff, 0);
+const composer = new EffectComposer(renderer);
 
 
-
-const renderPass = new RenderPass(scene, camera)
+const renderPass = new RenderPass(scene, camera);
 const outlinePass = new OutlinePass(new THREE.Vector2(parent.offsetWidth, parent.offsetHeight), scene, camera);
 outlinePass.edgeStrength = Number( 10 );
 outlinePass.edgeGlow = Number( 0);
@@ -30,11 +30,11 @@ camera.position.set(100, 0, 0);
 
 const controls = new OrbitControls(camera, renderer.domElement)
 
-const light = new THREE.SpotLight()
+const light = new THREE.SpotLight();
 const ambilight = new THREE.AmbientLight(0x444444);
 light.position.set(200, 200, 1000)
-scene.add(ambilight)
-scene.add(light)
+scene.add(ambilight);
+scene.add(light);
 
 var object, material;
 
@@ -88,7 +88,7 @@ function objectClickHandler(test) {
 	//console.log(count);
 	setForm(obj.ring, obj.crystal);
 }
-scene.background = new THREE.Color('#72c0ff');
+//scene.background = new THREE.Color('#72c0ff');
 
 renderer.setSize(parent.offsetWidth, parent.offsetHeight);
 parent.appendChild(renderer.domElement);
@@ -97,9 +97,12 @@ var mouse = new THREE.Vector2();
 function onDocumentMouseDown(event) {
 	//event.preventDefault();
 
-	const rect = event.target.getBoundingClientRect();
+	const rect = parent.getBoundingClientRect();
 	mouse.x = ((event.clientX - rect.left) / renderer.domElement.clientWidth) * 2 - 1;
 	mouse.y =  - ((event.clientY -rect.top) / renderer.domElement.clientHeight) * 2 + 1;
+	if(mouse.y < -1 || mouse.x < -1){
+		return
+	}
 	raycaster.setFromCamera(mouse, camera);
 	var intersects = raycaster.intersectObjects(full_list);
 	if (intersects.length > 0)
@@ -108,10 +111,12 @@ function onDocumentMouseDown(event) {
 function onDocumentMouseMove(event) {
 	//event.preventDefault();
  
-	const rect = event.target.getBoundingClientRect();
+	const rect = parent.getBoundingClientRect();
 	mouse.x = ((event.clientX - rect.left) / renderer.domElement.clientWidth) * 2 - 1;
 	mouse.y =  - ((event.clientY -rect.top) / renderer.domElement.clientHeight) * 2 + 1;
- 
+	if(mouse.y < -1 || mouse.x < -1){
+		return
+	}
     raycaster.setFromCamera(mouse, camera);
  
     var intersects = raycaster.intersectObjects(full_list);
